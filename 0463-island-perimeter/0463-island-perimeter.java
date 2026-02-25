49878a7101fc410dce695b5341c64ca6b46ff68b
MridhavKhajuria <148615907+MridhavKhajuria@users.noreply.github.com>
2026-02-25 12:09:57 +0530

    Time: 4 ms (99.9%), Space: 47.1 MB (81.63%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,41 +1,18 @@
 class Solution {
-    int m;
-    int n;
-    int cnt;
-    boolean[][] vis;
     public int islandPerimeter(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        cnt = 0;
-        vis = new boolean[m][n];
+        int m = grid.length;
+        int n = grid[0].length;
+        int perimeter = 0;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                    return cnt;
+                    perimeter += 4;
+                    if(i>0 && grid[i-1][j] == 1) perimeter -=2;
+                    if(j>0 && grid[i][j-1] == 1) perimeter -=2;
                 }
             }
         }
-        return cnt;
-    }
-
-    private void dfs(int[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i > m - 1 || j > n - 1) {
-            cnt++;
-            return;
-        }
-
-        if(grid[i][j] == 0){
-            cnt++;
-            return;
-        }
-
-        if(vis[i][j]) return;
-        vis[i][j] = true;
-        
-        dfs(grid, i+1 , j);
-        dfs(grid, i, j+1);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j-1);
+        return perimeter;
     }
 }
